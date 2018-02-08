9e0ae4c7ec37391b8e96646cfbb438691aad3505
Brittany Young <mrsbrityoung@gmail.com>
2018-02-08 22:19:53 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,3 +3,6 @@ def print_business_card(person)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
   puts "Contact: #{person[3]}"
 end
+
+bob = ["Bob", 17, "Chef", 367-5309]
+stacy = ["Stacy", 14, "Lady", 666-9999]
